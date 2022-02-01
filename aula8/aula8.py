53c659b950d3da2a508840d875232a99c2f272d1
Douglasakawe <douglasakawe@hotmail.com>
2022-02-01 18:12:13 -0400

    Entrada de dados

---
+++ aula8/aula8.py
@@ -0,0 +1,16 @@
+"""
+Entrada de dados - Aula 3
+"""
+
+#nome = input("Qual o seu nome? ")
+#print(f'O usuário digitou {nome} e o tipo da variável é '
+      #f'{type(nome)}')
+
+nome = input("Qual o seu nome? ")
+idade = input("Qual a sua idade? ")
+
+ano_nascimento = 2021-int(idade)
+
+print()
+print(f'{nome} tem {idade} anos. '
+      f'{nome} nasceu em {ano_nascimento}. ')
