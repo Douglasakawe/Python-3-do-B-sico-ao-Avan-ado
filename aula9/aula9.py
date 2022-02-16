3ad9d37e25d4861864b763a463e2ced88d4c7019
Douglasakawe <douglasakawe@hotmail.com>
2022-02-16 13:59:01 -0400

    IF, ELIF e ELSE + Booleans

---
+++ aula9/aula9.py
@@ -0,0 +1,19 @@
+"""
+Condições IF, ELIF e ELSE - Aula 9
+"""
+
+
+if False:
+    print('Verdadeiro.')
+    print('Testando Teste')
+elif True:
+    print("Verdadeiramente")
+    nome = input("Qual seu nome?")
+
+    print(f'Seu Nome é {nome}.')
+#elif False:
+#    print('Mais uma verdadeira.')
+#    print(16 + 16)
+#else:
+#    print("Não é verdadeiro.")
+#    print(" VAI SANTOS! ")
